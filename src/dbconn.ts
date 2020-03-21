941c04825c18c2ac9856ebc4c23efe1886cc1977
alokrajiv <mail@alokrajiv.com>
2020-03-22 02:39:30 +0800

    add metadata function

---
+++ src/dbconn.ts
@@ -0,0 +1,45 @@
+import { MongoClient, Db } from "mongodb";
+const MONGODB_URI = process.env.MONGODB_URI; // or Atlas connection string
+
+let cachedDb: Db = undefined;
+
+async function connectToDatabase (uri: string, dbName: string) {
+  console.log("=> connect to database");
+
+  if (cachedDb) {
+    console.log("=> using cached database instance");
+    return Promise.resolve(cachedDb);
+  }
+
+  const _client = await MongoClient.connect(uri);
+  cachedDb = _client.db(dbName);
+  return cachedDb;
+}
+
+async function queryDatabase (db: Db) {
+  console.log("=> query database");
+
+  try {
+    await db.collection("metadata").find({}).toArray();
+    return { statusCode: 200, body: "success" };
+  }
+  catch (err) {
+    console.log("=> an error occurred: ", err);
+    return { statusCode: 500, body: "error" };
+  }
+}
+
+exports.handler = async (event: any, context: any) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  console.log("event: ", event);
+
+  try {
+    const db = await connectToDatabase(process.env.MONGODB_URI, process.env.MONGODB_DBNAME);
+    const result = queryDatabase(db);
+    console.log("=> returning result: ", result);
+  } catch (error) {
+    console.log("=> an error occurred: ", error);
+    throw error;
+  }
+};
