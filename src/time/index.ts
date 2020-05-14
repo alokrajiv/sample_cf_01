af9eefe023705ce7a6d1e346207b2c789c8a95bc
alokrajiv <mail@alokrajiv.com>
2020-05-15 00:57:50 +0800

    change contents

--- src/time/index.ts
+++
@@ -1,9 +0,0 @@
-let time = require("time");
-exports.handler = async (event: any, context: any) => {
-    const currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
-    return {
-        statusCode: "200",
-        body: "The time in Los Angeles is: " + currentTime.toString(),
-    };
-};
